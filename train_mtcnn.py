87a0403eebaa6635f3c1cbd354f44a96626d7cb2
breadbread1984 <breadbread1984@163.com>
2019-09-02 17:25:01 +0800

    update parse function

--- train_mtcnn.py
+++ train_mtcnn.py
@@ -4,27 +4,28 @@ import cv2;
 import tensorflow as tf;
 from Model import MTCNN;
 
-def parse_function(serialized_example):
+def parse_widerface_function(serialized_example):
 
   feature = tf.io.parse_single_example(
     serialized_example,
     features = {
-      'data': tf.io.FixedLenFeature((), dtype = tf.string, default_value = ''),
-      'shape': tf.io.FixedLenFeature((), dtype = tf.int64, default_value = 0),
-      'objects': tf.io.FixedLenFeature((), dtype = tf.int64, default_value = 0),
-      'obj_num': tf.io.FixedLenFeature((), dtype = tf.int64, default_value = 0)
+      'data': tf.io.FixedLenFeature((), dtype = tf.string),
+      'shape': tf.io.FixedLenFeature((), dtype = tf.int64),
+      'objects': tf.io.FixedLenFeature((), dtype = tf.int64),
+      'obj_num': tf.io.FixedLenFeature((), dtype = tf.int64)
     }
-  };
-  shape = tf.reshape(feature['shape'],(2,));
+  );
+  shape = tf.reshape(feature['shape'], (3,));
   data = tf.io.decode_raw(feature['data'], out_type = tf.uint8);
-  data = tf.reshape(data, shape + [3,]);
-  obj_num = tf.reshape(feature['obj_num'], (1,));
-  objects = tf.reshape(feature['objects'], obj_num + [10,]);
+  data = tf.reshape(data, shape);
+  obj_num = tf.reshape(feature['obj_num'], ());
+  objects = tf.reshape(feature['objects'], (obj_num, 10));
   return data, objects;
 
 def main():
 
-  widerface_trainset = tf.data.TFRecordDataset('wider_face_train.tfrecord').map(parse_function).batch(1);
+  widerface_trainset = tf.data.TFRecordDataset('wider_face_train.tfrecord').map(parse_widerface_function).batch(1);
+  widerface_evalset = tf.data.TFRecordDataset('wider_face_val.tfrecord').map(parse_widerface_function).batch(1);
   for data, objects in widerface_trainset:
 
     cv2.imshow('debug',data[0].numpy());
