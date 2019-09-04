349d886f073f461dd8d842e03d39f29f5e3934c2
breadbread1984 <breadbread1984@163.com>
2019-09-04 09:33:25 +0800

    add celeba parse function

--- train_mtcnn.py
+++ train_mtcnn.py
@@ -23,10 +23,26 @@ def parse_widerface_function(serialized_example):
   objects = tf.reshape(objects, (obj_num, 10));
   return data, objects;
 
+def parse_celeba_function(serialized_example):
+
+  feature = tf.io.parse_single_example(
+    serialized_example,
+    features = {
+      'data': tf.io.FixedLenFeature((), dtype = tf.string),
+      'shape': tf.io.FixedLenFeature((3,), dtype = tf.int64),
+      'landmarks': tf.io.FixedLenFeature((10,2), dtype = tf.int64)
+    }
+  );
+  shape = tf.cast(feature['shape'], dtype = tf.int32);
+  data = tf.io.decode_raw(feature['data'], out_type = tf.uint8);
+  data = tf.reshape(data, shape);
+  landmarks = tf.cast(feature['landmarks'], dtype = tf.int32);
+  return data, landmarks;
+
 def main():
 
   widerface_trainset = tf.data.TFRecordDataset('wider_face_train.tfrecord').map(parse_widerface_function).batch(1);
-  widerface_evalset = tf.data.TFRecordDataset('wider_face_val.tfrecord').map(parse_widerface_function).batch(1);
+  celeba_trainset = tf.data.TFRecordDataset('celeba_train.tfrecord').map(parse_celeba_function).batch(1);
   for imgs, objects in widerface_trainset:
 
     img = imgs[0].numpy();
